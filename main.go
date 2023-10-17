1c16090052ff321e2ad25a8c507c528723ceab96
Aleksandr Zakharov <azakharov@genetec.com>
2023-10-17 13:43:42 +0200

    Added is balanced solution

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"algos/recursion"
+	"algos/trees"
 	"fmt"
 )
 
 func main() {
-	result := recursion.RecursiveFileSearch("..//")
-	for _, entry := range result {
-		fmt.Println(entry.Name())
-	}
+	root := trees.TreeNode{Val: 3}
+	nineNode := trees.TreeNode{Val: 9}
+	twentyNode := trees.TreeNode{Val: 20}
+	fifteenNode := trees.TreeNode{Val: 15}
+	sevenNode := trees.TreeNode{Val: 7}
+	root.Left = &nineNode
+	root.Right = &twentyNode
+	twentyNode.Left = &fifteenNode
+	twentyNode.Right = &sevenNode
+	fmt.Println(trees.IsBalanced(&root))
 }
