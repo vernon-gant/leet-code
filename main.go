6f6de0e85ae54a67bebc39f95d73483166176be1
Aleksandr Zakharov <azakharov@genetec.com>
2024-03-22 12:43:43 +0100

    Added merge intervals solution

--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"algos/sliding_window"
+	"algos/merge_intervals"
 	"fmt"
 )
 
 func main() {
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-	result := sliding_window.FindRepeatedDnaSequences("AAAAAAAAAAA")
-=======
-	result := sliding_window.MaxSlidingWindow([]int{7,2,4},2)
->>>>>>> Stashed changes
+	result := merge_intervals.MergeIntervals([][]int{{1,4}, {0,0}})
 	fmt.Print(result)
-=======
-	repeated := sliding_window.FindRepeatedSequences("AGAGCTCCAGAGCTTG",6)
-	fmt.Print(repeated)
->>>>>>> 4736469c001b27782897c622439e36268ec2ae53
 }
