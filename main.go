fc798daf65b2124ab5bb48698f72f0ffaaed4d6c
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-04-01 12:49:52 +0200

    Task Schedult

--- main.go
+++ main.go
@@ -6,25 +6,6 @@ import (
 )
 
 func main() {
-	scheduleData := [][][]int{
-		{{1, 3}, {6, 7}},
-		{{2, 4}},
-		{{2, 5}, {9, 12}},
-	}
-
-	schedule := make([][]*merge_intervals.Interval, len(scheduleData))
-	for i, empSchedule := range scheduleData {
-		schedule[i] = make([]*merge_intervals.Interval, len(empSchedule))
-		for j, iv := range empSchedule {
-			interval := &merge_intervals.Interval{}
-			interval.IntervalInit(iv[0], iv[1])
-			schedule[i][j] = interval
-		}
-	}
-
-	freeTimes := merge_intervals.EmployeeFreeTime(schedule)
-
-	for _, iv := range freeTimes {
-		fmt.Println(iv.Str())
-	}
+	leastInterval := merge_intervals.LeastInterval([]byte{'A','C','A','B','D','B'},1)
+	fmt.Print(leastInterval)
 }
