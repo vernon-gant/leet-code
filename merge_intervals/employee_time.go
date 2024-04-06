4cc95bd2719addb20c7a671ca1f725ffed61545f
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-04-06 12:53:24 +0200

    Fine tune employee time

--- merge_intervals/employee_time.go
+++ merge_intervals/employee_time.go
@@ -1,6 +1,7 @@
 package merge_intervals
 
 import (
+	"math"
 	"sort"
 	"strconv"
 )
@@ -25,6 +26,9 @@ func EmployeeFreeTime(schedule [][]*Interval) []*Interval {
 	for _, singleSchedule := range schedule {
 		allIntervals = append(allIntervals, singleSchedule...)
 	}
+	if len(allIntervals) == 1 || len(allIntervals) == 0 {
+		return allIntervals
+	}
 	sort.Slice(allIntervals, func(i, j int) bool {
 		if allIntervals[i].Start == allIntervals[j].Start {
 			return allIntervals[i].End < allIntervals[j].End
@@ -32,16 +36,17 @@ func EmployeeFreeTime(schedule [][]*Interval) []*Interval {
 		return allIntervals[i].Start < allIntervals[j].Start
 	})
 	var result []*Interval
-	for i := 0; i < len(allIntervals) - 1; i++ {
-		if allIntervals[i+1].Start <= allIntervals[i].End {
-			allIntervals[i + 1].Start = min(allIntervals[i].Start,allIntervals[i+1].Start)
-			allIntervals[i + 1].End = max(allIntervals[i].End,allIntervals[i+1].End)
+	currentIntervalEnd := math.MinInt
+	for i := 1; i < len(allIntervals); i++ {
+		if allIntervals[i].Start <= allIntervals[i-1].End {
+			currentIntervalEnd = max(allIntervals[i-1].End, allIntervals[i].End)
 			continue
 		}
 		result = append(result, &Interval{
-			Start: allIntervals[i].End,
-			End:   allIntervals[i+1].Start,
+			Start: currentIntervalEnd,
+			End:   allIntervals[i].Start,
 		})
+		currentIntervalEnd = allIntervals[i].End
 	}
 	return result
 }
