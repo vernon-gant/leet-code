94a192d24546e01ab63a5f4cf66687569cdef0be
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-04-06 12:00:15 +0200

    Meeting rooms 2 solution

--- merge_intervals/meeting_rooms_2.go
+++ merge_intervals/meeting_rooms_2.go
@@ -1,6 +1,41 @@
 package merge_intervals
 
-import "sort"
+import (
+	"container/heap"
+	"sort"
+)
+
+type MinHeap []int
+
+func (minHeap* MinHeap) Len() int  {
+	return len(*minHeap)
+}
+
+func (minHeap* MinHeap) Less(i, j int) bool  {
+	return (*minHeap)[i] < (*minHeap)[j]
+}
+
+func (minHeap* MinHeap) Swap(i, j int)  {
+	(*minHeap)[i], (*minHeap)[j] = (*minHeap)[j], (*minHeap)[i]
+}
+
+func (minHeap* MinHeap) Push(x interface{})  {
+	*minHeap = append(*minHeap, x.(int))
+}
+
+func (minHeap* MinHeap) Pop() interface{}  {
+	heapLen := len(*minHeap)
+	last := (*minHeap)[heapLen - 1]
+	*minHeap = (*minHeap)[0:heapLen - 1]
+	return last
+}
+
+func (minHeap * MinHeap) Peek() interface{}  {
+	if minHeap.Len() == 0 {
+		panic(minHeap)
+	}
+	return (*minHeap)[0]
+}
 
 func findSets(intervals [][]int) int{
 	sort.Slice(intervals, func(i, j int) bool {
@@ -9,20 +44,12 @@ func findSets(intervals [][]int) int{
 		}
 		return intervals[i][0] < intervals[j][0]
 	})
-	meetingEndTimes := []int{intervals[0][1] }
+	meetingEndTimes := &MinHeap{ intervals[0][1] }
 	for _, interval := range intervals[1:] {
-		roomFound := false
-		for i := 0; i < len(meetingEndTimes); i++ {
-			if interval[0] >= meetingEndTimes[i] {
-				meetingEndTimes[i] = interval[0]
-				roomFound = true
-				break
-			}
+		if interval[0] >= meetingEndTimes.Peek().(int) {
+			heap.Pop(meetingEndTimes)
 		}
-		if roomFound {
-			continue
-		}
-		meetingEndTimes = append(meetingEndTimes, interval[1])
+		heap.Push(meetingEndTimes,interval[1])
 	}
-	return len(meetingEndTimes)
+	return meetingEndTimes.Len()
 }
