6e011cfddecda03a6a6a1bdf6066db6753279c66
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-04-24 10:29:17 +0200

    Added for educative

--- in_place_list_manipulation/reverse_list_k_groups.go
+++ in_place_list_manipulation/reverse_list_k_groups.go
@@ -28,3 +28,34 @@ func findNextGroupStart(head, currentPointer *ListNode, currentSize, k int) (*Li
 	}
 	return findNextGroupStart(head, currentPointer.Next, currentSize+1, k)
 }
+
+////////////////////////////////////////////////////////
+
+func reverseKGroups(head *EduLinkedListNode, k int) *EduLinkedListNode {
+	reversed := &EduLinkedListNode{}
+	return reverseKGroupHelperEdu(reversed, reversed, head, k)
+}
+
+func reverseKGroupHelperEdu(reversed, reversedTail, head *EduLinkedListNode, k int) *EduLinkedListNode {
+	nextGroupStart, currentGroupSize := findNextGroupStartEdu(head, head, 1, k)
+	if currentGroupSize == k {
+		reversedTail.next = reverseHelper(nil, head)
+	}
+	if nextGroupStart == nil {
+		return reversed.next
+	}
+	head.next = nextGroupStart
+	return reverseKGroupHelperEdu(reversed, head, nextGroupStart, k)
+}
+
+func findNextGroupStartEdu(head, currentPointer *EduLinkedListNode, currentSize, k int) (*EduLinkedListNode, int) {
+	if currentPointer == nil {
+		return nil, -currentSize
+	}
+	if currentSize == k {
+		nextGroupStart := currentPointer.next
+		currentPointer.next = nil
+		return nextGroupStart, k
+	}
+	return findNextGroupStartEdu(head, currentPointer.next, currentSize+1, k)
+}
