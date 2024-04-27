5134de544c8d9a6f3f07c1219e784fe953d2276d
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-04-27 15:59:17 +0200

    Educative

--- in_place_list_manipulation/swap_nodes.go
+++ in_place_list_manipulation/swap_nodes.go
@@ -16,3 +16,22 @@ func swapNodesHelper(head, current, front, end *ListNode, counter, k int) *ListN
 	}
 	return swapNodesHelper(head, current.Next, front, end.Next, counter + 1, k)
 }
+
+///
+
+func SwapNodesEdu(head *EduLinkedListNode, k int) *EduLinkedListNode {
+	return swapNodesHelperEdu(head, head, head, head, 1, k)
+}
+
+func swapNodesHelperEdu(head, current, front, end *EduLinkedListNode, counter, k int) *EduLinkedListNode {
+	if counter == k {
+		return swapNodesHelperEdu(head, current.next, current, head, counter + 1, k)
+	}
+	if current == nil {
+		frontVal := front.data
+		front.data = end.data
+		end.data = frontVal
+		return head
+	}
+	return swapNodesHelperEdu(head, current.next, front, end.next, counter + 1, k)
+}
