e47d93a5589cbd00c3cdf774a6246386e5fe9b85
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-05-03 14:17:09 +0200

    Median data stream

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
-	result := two_heaps.FindMaximizedCapital(2,0,[]int{1,2,3},[]int{0,9,10})
-	fmt.Print(result)
+	medianFinder := two_heaps.Constructor()
+	medianFinder.AddNum(1);    // arr = [1]
+	medianFinder.AddNum(2);    // arr = [1, 2]
+	fmt.Println(medianFinder.FindMedian()) // return 1.5 (i.e., (1 + 2) / 2)
+	medianFinder.AddNum(3);    // arr[1, 2, 3]
+	fmt.Println(medianFinder.FindMedian())
 }
--- two_heaps/my_heap.go
+++
@@ -1,7 +0,0 @@
-package two_heaps
-
-type MyHeap struct {
-	count int
-	elements []interface{}
-}
-
