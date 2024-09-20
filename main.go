7cb568147c4d240315cc0a2b71e4f83ea1b9cfbd
Aleksandr Zakharov <azakharov@genetec.com>
2024-09-20 17:04:45 +0200

    Diameter Of BinaryTree

--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-import "algos/greedy"
+import "algos/binary_tree"
 
 func main() {
-    result := greedy.Jump2([]int{1,1,1,1,1})
+    root := &binary_tree.TreeNode{
+        Val : 1,
+        Left: &binary_tree.TreeNode{
+            Val : 2,
+            Left : &binary_tree.TreeNode {
+                Val: 4,
+                Left: &binary_tree.TreeNode{
+                    Val: 7,
+                },
+            },
+            Right: &binary_tree.TreeNode{
+                Val: 5,
+                Right: &binary_tree.TreeNode{
+                    Val: 8,
+                    Left: &binary_tree.TreeNode{
+                        Val: 10,
+                    },
+                    Right: &binary_tree.TreeNode{
+                        Val: 11,
+                        Right: &binary_tree.TreeNode{
+                            Val: 13,
+                        },
+                    },
+                },
+            },
+        },
+        Right: &binary_tree.TreeNode{
+            Val: 3,
+            Right: &binary_tree.TreeNode{
+                Val: 6,
+                Right: &binary_tree.TreeNode{
+                    Val: 9,
+                    Right: &binary_tree.TreeNode{
+                        Val: 12,
+                    },
+                },
+            },
+        },
+    }
+    result := binary_tree.DiameterOfBinaryTree2(root)
     print(result)
 }
