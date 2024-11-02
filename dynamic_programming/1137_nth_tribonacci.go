9ad9109bd00d107736ecdc646783f5f0116c6405
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-11-02 09:48:11 +0100

    1137

---
+++ dynamic_programming/1137_nth_tribonacci.go
@@ -0,0 +1,21 @@
+package dynamic_programming
+
+func tribonacci(n int) int {
+    if n == 0 {
+        return 0
+    }
+
+    if n == 1 || n == 2 {
+        return 1
+    }
+
+    dp := make([]int, n + 1)
+    dp[1] = 1
+    dp[2] = 1
+
+    for i := 3; i <= n; i++ {
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+    }
+
+    return dp[n]
+}
