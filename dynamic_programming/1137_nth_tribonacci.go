e2ade7bfceb2d4e99ad32ff89d4dd7888e1da024
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-11-02 10:04:43 +0100

    1137 updated

--- dynamic_programming/1137_nth_tribonacci.go
+++ dynamic_programming/1137_nth_tribonacci.go
@@ -9,13 +9,15 @@ func tribonacci(n int) int {
         return 1
     }
 
-    dp := make([]int, n + 1)
-    dp[1] = 1
-    dp[2] = 1
+    first, second, third := 0, 1, 1
+	var temp int
 
-    for i := 3; i <= n; i++ {
-        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
-    }
+	for i := 3; i <= n; i++ {
+		temp = first + second + third
+		first = second
+		second = third
+		third = temp
+	}
 
-    return dp[n]
+	return third
 }
