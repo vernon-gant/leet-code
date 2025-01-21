99291304158aa2f160f8ec470131ec968b70b5b9
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2025-01-21 10:39:46 +0100

    136 single number

--- bit_manipulation/389_find_the_difference.cs
+++ bit_manipulation/389_find_the_difference.cs
@@ -1,5 +1,7 @@
-public class Solution {
-    public static int ExtraCharacterIndex(String str1, String str2) {
+public class Solution
+{
+    public static int ExtraCharacterIndex(String str1, String str2)
+	{
         int result = 0;
 
 	    foreach(char letter in t)
