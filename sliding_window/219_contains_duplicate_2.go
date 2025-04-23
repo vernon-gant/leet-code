4d24f11cae18d62cefd8a1f421badb917c5683a0
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2025-04-23 18:50:02 +0300

    219 update

--- sliding_window/219_contains_duplicate_2.go
+++ sliding_window/219_contains_duplicate_2.go
@@ -2,20 +2,14 @@ package sliding_window
 
 func containsNearbyDuplicate(nums []int, k int) bool {
     windowFreq := make(map[int]bool)
-    for i := 0; i < len(nums) && i <= k; i++ {
-        inWindow, found := windowFreq[nums[i]]
-        if found && inWindow {
+    for i := 0; i < len(nums); i++ {
+        if inWindow, found := windowFreq[nums[i]]; found && inWindow {
             return true
         }
         windowFreq[nums[i]] = true
-    }
-    for i := 1; i + k < len(nums); i++ {
-        windowFreq[nums[i - 1]] = false
-        inWindow, found := windowFreq[nums[i + k]]
-        if found && inWindow {
-            return true
+        if len(windowFreq) > k {
+            delete(windowFreq, nums[i - k])
         }
-        windowFreq[nums[i + k]] = true
     }
     return false
 }
